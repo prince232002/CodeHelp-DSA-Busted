c064f534b10279c522789cba87d53e21330c5c53
prince232002 <prince2002gupta@gmail.com>
2022-04-05 10:57:26 +0530

    Gitacora saved 5 changed files

--- tstt/t1.cpp
+++ tstt/t1.cpp
@@ -17,10 +17,5 @@ int main()
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
- int n; 
- cout <<"enter the number ";
- cin>> n;
- for(int i=0; i<n; i++){
-     cout<< n << i << "fsfss";
  }
 }
